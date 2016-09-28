9053eeec0f2c88a78790491688bb71f3f4be193d
Moxikai <zhu-hero@qq.com>
2016-09-28 15:27:07 +0800

    v0.1

--- ppeye/settings.py
+++ ppeye/settings.py
@@ -84,7 +84,7 @@ ROBOTSTXT_OBEY = False
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 3600
+HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
--- ppeye/spiders/p2peye.py
+++ ppeye/spiders/p2peye.py
@@ -1,28 +1,52 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
+
 from scrapy import Spider
 from scrapy import Request
 from scrapy_splash import SplashRequest
 from bs4 import BeautifulSoup
+
 class P2peyeSpider(Spider):
     name = "p2peye"
     #allowed_domains = ["p2peye.com"]
     start_urls = (
         'http://www.p2peye.com/rating',
     )
-
+    headers = {
+        'Host':'www.p2peye.com',
+        'Upgrade-Insecure-Requests':'1',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Referer':'http://www.p2peye.com/',
+        'Accept-Encoding':'gzip, deflate, sdch',
+        'Accept-Language':'zh-CN,zh;q=0.8',
+    }
     def start_requests(self):
         """自定义起始request"""
         for url in self.start_urls:
 
-            yield SplashRequest(url, self.parse, args={'wait': 1})
+            yield SplashRequest(url, self.parse,
+                                args={'wait': 10},
+                                splash_headers=self.headers)
 
     def parse(self, response):
         """解析列表页"""
         #print response.body
         soup = BeautifulSoup(response.body,'html.parser')
+        headers = deepcopy(self.headers)
+        headers['Referer']='http://www.p2peye.com/rating'
         td_list = soup.find_all('td',class_='name')
-        result = [{'pt_url':td.get('href'),
-                   'pt_name':td.get_text(),} for td in td_list]
+        result = [{'pt_url':td.a.get('href'),
+                   'pt_name':td.a.get_text(),} for td in td_list]
+        for item in result:
+            print item['pt_name'],item['pt_url']
+
+            yield SplashRequest(url=item['pt_url'],
+                                callback=self.parseDetail,
+                                args={'wait':10},
+                                splash_headers=headers,
+                                )
+
 
     def parseDetail(self,response):
         """解析详细页面"""
